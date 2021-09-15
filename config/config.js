3ed3147445fb2da142228f488d110a076df25a02
Vaibhav Gope <vaibhavgope02@gmail.com>
2021-09-15 11:45:30 +0530

    Changes in environment variable

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
+const key = process.env.API_KEY
+
 const config = {
-    APIEndpoint: "https://api.themoviedb.org/3/movie/popular?api_key=12d0d0152e46f86ae03b9f7e6f83c85a",
-    imgEndpoint: "https://image.tmdb.org/t/p/original",
-    searchEndpoint: (term) => `https://api.themoviedb.org/3/search/movie?api_key=12d0d0152e46f86ae03b9f7e6f83c85a&query=${term}`,
-    singleMovieEndpoint: (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}?api_key=12d0d0152e46f86ae03b9f7e6f83c85a&language=en-US`,
-    genresEndpoint: 'https://api.themoviedb.org/3/genre/movie/list?api_key=12d0d0152e46f86ae03b9f7e6f83c85a&language=en-US',
-    similarTitlesEndpoint: (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=12d0d0152e46f86ae03b9f7e6f83c85a&language=en-US&page=1`
+    APIEndpoint: `https://api.themoviedb.org/3/movie/popular?api_key=${key}`,
+    imgEndpoint: `https://image.tmdb.org/t/p/original`,
+    searchEndpoint: (term) => `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${term}`,
+    singleMovieEndpoint: (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${key}&language=en-US`,
+    genresEndpoint: `https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=en-US`,
+    similarTitlesEndpoint: (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${key}&language=en-US&page=1`
 };
 
 export default config;
